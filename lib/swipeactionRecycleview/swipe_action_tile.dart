2dad429aaa4801a1bffe0935bf622941daa4235b
AkshaySarkale <akshaysarkale999@gmail.com>
2026-06-05 15:19:31 +0530

    fix overflow issue

--- lib/swipeactionRecycleview/swipe_action_tile.dart
+++ lib/swipeactionRecycleview/swipe_action_tile.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:library_flutter_swipeaction_recycleview/swipeactionRecycleview/swipe_action.dart';
 
 class CustomSwipeActionTile extends StatefulWidget {
   final Widget child;
   final List<SwipeAction> leftActions;
   final List<SwipeAction> rightActions;
+  final double actionWidth;
+  final double dragResistance;
 
   const CustomSwipeActionTile({
     super.key,
     required this.child,
-    required this.leftActions,
-    required this.rightActions,
+    this.leftActions = const [],
+    this.rightActions = const [],
+    this.actionWidth = 80.0,
+    this.dragResistance = 0.8,
   });
 
   @override
@@ -31,6 +36,24 @@ class _CustomSwipeActionTileState extends State<CustomSwipeActionTile>
       vsync: this,
       duration: const Duration(milliseconds: 250),
     );
+
+    _controller.addListener(() {
+      if (_animation != null) {
+        setState(() {
+          _offset = _animation!.value;
+        });
+      }
+    });
+  }
+
+  @override
+  void didUpdateWidget(covariant CustomSwipeActionTile oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    // Reset offset if the widget is reused for a different item in a list
+    if (widget.child != oldWidget.child) {
+      _offset = 0;
+      _controller.stop();
+    }
   }
 
   @override
@@ -40,79 +63,56 @@ class _CustomSwipeActionTileState extends State<CustomSwipeActionTile>
   }
 
   void _animateTo(double target) {
-    _animation = Tween<double>(begin: _offset, end: target).animate(
-      CurvedAnimation(parent: _controller, curve: Curves.easeOutCubic),
-    )..addListener(() {
-      setState(() {
-        _offset = _animation!.value;
-      });
-    });
+    _controller.stop();
+    _animation = Tween<double>(
+      begin: _offset,
+      end: target,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeOutCubic,
+    ));
     _controller.forward(from: 0);
+  }
+
+  void _handleActionTap(SwipeAction action) async {
+    HapticFeedback.mediumImpact();
+    _animateTo(0);
+    // Brief delay for visual smoothness before triggering action
+    await Future.delayed(const Duration(milliseconds: 150));
+    action.onTap();
   }
 
   @override
   Widget build(BuildContext context) {
-    const double actionWidth = 80.0;
-    final double maxRightOffset = -(widget.rightActions.length * actionWidth);
-    final double maxLeftOffset = (widget.leftActions.length * actionWidth);
+    final double maxRightOffset = -(widget.rightActions.length * widget.actionWidth);
+    final double maxLeftOffset = (widget.leftActions.length * widget.actionWidth);
 
     return Stack(
       children: [
-        // Actions Layer
         Positioned.fill(
           child: LayoutBuilder(
             builder: (context, constraints) {
               return Stack(
                 children: [
-                  // Left Actions (reveal when swiping right)
-                  if (_offset > 0)
+                  if (_offset > 0 && widget.leftActions.isNotEmpty)
                     Positioned(
                       left: 0,
                       top: 0,
                       bottom: 0,
                       child: Row(
                         children: widget.leftActions.map((action) {
-                          return Material(
-                            color: action.color,
-                            child: InkWell(
-                              onTap: () {
-                                _animateTo(0);
-                                action.onTap();
-                              },
-                              child: SizedBox(
-                                width: actionWidth,
-                                child: Center(
-                                  child: Icon(action.icon, color: Colors.white),
-                                ),
-                              ),
-                            ),
-                          );
+                          return _buildAction(action);
                         }).toList(),
                       ),
                     ),
-                  // Right Actions (reveal when swiping left)
-                  if (_offset < 0)
+                  if (_offset < 0 && widget.rightActions.isNotEmpty)
                     Positioned(
                       right: 0,
                       top: 0,
                       bottom: 0,
                       child: Row(
                         children: widget.rightActions.map((action) {
-                          return Material(
-                            color: action.color,
-                            child: InkWell(
-                              onTap: () {
-                                _animateTo(0);
-                                action.onTap();
-                              },
-                              child: SizedBox(
-                                width: actionWidth,
-                                child: Center(
-                                  child: Icon(action.icon, color: Colors.white),
-                                ),
-                              ),
-                            ),
-                          );
+                          return _buildAction(action);
                         }).toList(),
                       ),
                     ),
@@ -121,7 +121,6 @@ class _CustomSwipeActionTileState extends State<CustomSwipeActionTile>
             },
           ),
         ),
-        // Child Layer
         Transform.translate(
           offset: Offset(_offset, 0),
           child: GestureDetector(
@@ -132,18 +131,33 @@ class _CustomSwipeActionTileState extends State<CustomSwipeActionTile>
               }
             },
             onHorizontalDragUpdate: (details) {
+              if (_controller.isAnimating) _controller.stop();
+              
               setState(() {
-                _offset += details.delta.dx;
-                // Clamp offset
+                _offset += details.delta.dx * widget.dragResistance;
+                
+                // Clamp and allow slight overscroll resistance
                 if (_offset < maxRightOffset) {
-                  _offset = maxRightOffset;
+                  _offset = maxRightOffset + (details.delta.dx * 0.1);
                 } else if (_offset > maxLeftOffset) {
-                  _offset = maxLeftOffset;
+                  _offset = maxLeftOffset + (details.delta.dx * 0.1);
                 }
+                
+                // Hard clamp for sanity
+                _offset = _offset.clamp(
+                  maxRightOffset - 20, 
+                  maxLeftOffset + 20
+                );
               });
             },
             onHorizontalDragEnd: (details) {
-              if (_offset < maxRightOffset / 2) {
+              final velocity = details.primaryVelocity ?? 0;
+              
+              if (velocity < -300) {
+                _animateTo(maxRightOffset);
+              } else if (velocity > 300) {
+                _animateTo(maxLeftOffset);
+              } else if (_offset < maxRightOffset / 2) {
                 _animateTo(maxRightOffset);
               } else if (_offset > maxLeftOffset / 2) {
                 _animateTo(maxLeftOffset);
@@ -155,6 +169,24 @@ class _CustomSwipeActionTileState extends State<CustomSwipeActionTile>
           ),
         ),
       ],
+    );
+  }
+
+  Widget _buildAction(SwipeAction action) {
+    return Material(
+      color: action.color,
+      child: InkWell(
+        onTap: () => _handleActionTap(action),
+        child: SizedBox(
+          width: widget.actionWidth,
+          child: Center(
+            child: Icon(
+              action.icon,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
